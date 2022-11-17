32c6179fd6a9c7719a845501921a63f9d9af7172
andreagy <andi.gyimoti@gmail.com>
2022-11-17 20:29:47 +0200

    working on exercise 3 and 4

--- Module_1/exercise_3.js
+++ Module_1/exercise_3.js
@@ -1,23 +1,15 @@
-'use strict';
+let number1 = prompt("Give me a number!");
+let number2 = prompt("Give me a second number!");
+let number3 = prompt("Give me a third number!");
 
-let number1 = prompt("Give me a number!")
-let number2 = prompt("Give me a second number!")
-let number3 = prompt("Give me a third number!")
-
-const n1 = parseInt(number1)
-const n2 = parseInt(number2)
-const n3 = parseInt(number3)
+const n1 = parseInt(number1);
+const n2 = parseInt(number2);
+const n3 = parseInt(number3);
 
 let sum = (n1 + n2 + n3);
+let product = (n1 * n2 * n3);
 let average = (sum / 3);
 
-document.write("Sum of the numbers is: " + sum);
-document.write("Average of the numbers is: " + average);
-
- /*
-
-const firstNum = +prompt("Enter first number!")
-
-document.querySelector('#sum').InnerHTML = 'The sum is ${firstNum}';
-
- */
+document.querySelector('#sum').innerHTML = `The sum is ${sum}`;
+document.querySelector('#product').innerHTML = `The product is ${product}`;
+document.querySelector('#average').innerHTML = `The average is ${average}`;
---
+++ Module_1/exercise_4.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const studentName = prompt("Give your name to the Sorting Hat");
+let result = `${studentName}, now you are in the class.`
+const sort = Math.floor(Math.random() * 4) +1;
+switch (sort) {
+  case
+}
