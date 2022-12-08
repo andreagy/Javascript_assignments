8881b0da493128bc7872feb866894410fe446f40
andreagy <andi.gyimoti@gmail.com>
2022-12-08 22:21:16 +0200

    Completing module 4

---
+++ Module_4/s1/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+document.addEventListener('submit', async function(evt) {
+    evt.preventDefault();
+    const inputValue = document.querySelector('#input').value
+    try {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const search = 'https://api.tvmaze.com/search/shows?q='+ inputValue;
+        const url = proxy + (search)
+        console.log(url)
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Invalid input!');
+        const json = await response.json();
+        console.log('result', json);
+    } catch (e) {
+        console.log('error', e);
+    }
+});
